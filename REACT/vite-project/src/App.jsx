c053a68c0a99610a7503e3e952e46d27ab816deb
husnasaadi <160035721+husnasaadi@users.noreply.github.com>
2024-06-29 17:56:01 +0500

    Add files via upload

---
+++ REACT/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+const App=()=>{
+  return(<div>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quaerat natus fuga ratione vel nulla inventore. Necessitatibus autem est dolorum, vero mollitia dolor quae suscipit ullam deserunt quidem, reprehenderit quis.</p>
+    <img src="/vite.svg" alt="" />
+  </div>  )
+}
+export{App}
